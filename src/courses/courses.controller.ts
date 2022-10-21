98cdbdec3dd158114211c6dcfa4bfc131e35366d
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-10-21 16:52:37 -0300

    feat(src/courses/courses.controller.ts): use service.

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -10,9 +10,12 @@ import {
     Post,
     Res,
 } from '@nestjs/common'
+import { CoursesService } from './courses.service'
 
 @Controller('courses')
 export class CoursesController {
+    constructor(private readonly coursesService: CoursesService) {}
+
     @Get()
     // Podemos manipular o Status Code das respostas, utilizando o decorator @Res()
     findAll(@Res() response) {
